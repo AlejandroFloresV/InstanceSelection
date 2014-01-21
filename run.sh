4563f799ffc7f0eff8477f20b6d20efc46aa70ab
AlejandroFloresV <alejandroflores.af@gmail.com>
2014-01-21 18:21:54 -0430

    Other changes

--- run.sh
+++ run.sh
@@ -13,12 +13,15 @@ for i in {0..9}
 do
 	for j in {1..5}
 	do
-		echo -en "\rSet #$i - Run #$j"
-		(time ./run $1 data/$2.norm $i) >> output/rawout 2>&1
+		echo -en "\rSet #$((i+1)) - Run #$j"
+		(time ./run $1 data/$2.norm $i) >> output/$1-$2.rawout 2>&1
 	done
 done
 
+echo "sadad" >> output/$1-$2.rawout
+
 echo -e "\rParsing output..."
-output/parse < output/rawout > output/out
-rm output/rawout
+output/parse < output/$1-$2.rawout > output/$1-$2.out
+rm output/$1-$2.rawout
+echo "Saved at output/$1-$2.out"
 echo "Done."
