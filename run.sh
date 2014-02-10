653cfde39bafaacc82f77acc4a37c5c04ba5f8ed
AlejandroFloresV <alejandroflores.af@gmail.com>
2014-02-09 22:06:30 -0430

    Full Factorial Experiment uses cool arguments too !

--- run.sh
+++ run.sh
@@ -1,27 +1,23 @@
 #!/bin/bash
 
-if [ $# -ne 2 ]
-then
-	echo "Usage: <algorithm> <set>"
-	exit $E_BADARGS
-fi
-
 echo "Full Factorial Experiment"
 echo "Running..."
 
 mkdir -p output
+rm -f output/result.rawout
 
 for i in {0..9}
 do
 	for j in {1..5}
 	do
 		echo -en "\rSet #$((i+1)) - Run #$j"
-		(time bin/solver $1 data/$2.norm $i) >> output/$1-$2.rawout 2>&1
+		(time bin/solver $@ -10fcv $i -nv) >> output/result.rawout 2>&1
+		sleep 2
 	done
 done
 
 echo -e "\rParsing output..."
-bin/parseOutput < output/$1-$2.rawout > output/$1-$2.out
-rm output/$1-$2.rawout
-echo "Saved at output/$1-$2.out"
+bin/parseOutput < output/result.rawout > output/result.out
+rm output/result.rawout
+echo "Saved at output/result.out"
 echo "Done."
