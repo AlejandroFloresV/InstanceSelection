fa86bc57e9235f839b4d25d9d13522a5f21b533b
AlejandroFloresV <alejandroflores.af@gmail.com>
2014-02-13 18:07:03 -0430

    Changes

--- full.sh
+++ full.sh
@@ -3,21 +3,23 @@
 echo "Full Factorial Experiment"
 echo "Running..."
 
-mkdir -p output
-rm -f output/result.rawout
+rawOut="rawout.txt"
+endOut="output.txt"
+rm -f $rawOut
 
 for i in {0..9}
 do
 	for j in {1..5}
 	do
 		echo -en "\rSet #$((i+1)) - Run #$j"
-		(time bin/solver $@ -10fcv $i -nv) >> output/result.rawout 2>&1
+		(time bin/solver $@ -10fcv $i -nv) >> $rawOut 2>&1
 		sleep 2
 	done
 done
 
 echo -e "\rParsing output..."
-bin/parseOutput < output/result.rawout > output/result.out
-rm output/result.rawout
-echo "Saved at output/result.out"
+echo "bin/solver $@" >> $endOut
+bin/parseOutput < $rawOut >> $endOut
+rm $rawOut
+echo "Saved at $endOut"
 echo "Done."
