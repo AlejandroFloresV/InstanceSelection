2801f085bd987b6206698097a6faad6bdfd974f7
AlejandroFloresV <alejandroflores.af@gmail.com>
2014-08-07 09:15:48 -0430

    +gga bash

---
+++ gga.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+alg=GGA
+options="-alg $alg -iter 1000 -pop 50 -cross 0.9"
+inits=("Random" "CNN" "ClosestNE" "FarthestNE" "NEHS")
+
+for file in `ls data/*.csv`
+do
+	l=`expr ${#file} - 9`
+	f=${file:5:$l}
+	for init in ${inits[@]}
+	do
+		./fold.sh test/$alg-$f-$init.csv $options -f $file -init $init
+	done
+done
