d22fe36a7d4a11b2d9438824b1b3524f434f0d6a
AlejandroFloresV <alejandroflores.af@gmail.com>
2014-08-25 08:57:00 -0430

    Aja

--- pbil.sh
+++ pbil.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
 alg=PBIL
-options="-alg $alg -iter 1000 -pop 40 -lr 0.1 -nlr 0.01"
+options="-alg $alg -iter 1000 -pop 40 -lr 0.1 -nlr 0.01 -bit 0.25"
 inits=("Random" "CNN" "ClosestNE" "FarthestNE" "NEHS")
 
-for file in `ls data/*.csv`
-do
-	l=`expr ${#file} - 9`
-	f=${file:5:$l}
-	for init in ${inits[@]}
-	do
-		./fold.sh test/$alg-$f-$init.csv $options -f $file -init $init
-	done
-done
+./fold.sh test/$alg-$1-Unif25.csv $options -f $file
+
+#for file in `ls data/*.csv`
+#do
+#	l=`expr ${#file} - 9`
+#	f=${file:5:$l}
+#	for init in ${inits[@]}
+#	do
+#		./fold.sh test/$alg-$f-$init.csv $options -f $file -init $init
+#	done
+#done
--- pso.sh
+++ pso.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
 alg=PSO
-options="-alg $alg -iter 1000 -pop 5 -pso 5 -w 0.9 -c1 0.1 -c2 0.1 -vmax 0.2"
+options="-alg $alg -iter 1000 -pop 5 -pso 5 -w 0.9 -c1 0.1 -c2 0.1 -vmax 0.2 -bit 0.25"
 inits=("Random" "CNN" "ClosestNE" "FarthestNE" "NEHS")
 
-for file in `ls data/*.csv`
-do
-	l=`expr ${#file} - 9`
-	f=${file:5:$l}
-	for init in ${inits[@]}
-	do
-		./fold.sh test/$alg-$f-$init.csv $options -f $file -init $init
-	done
-done
+./fold.sh test/$alg-$1-Unif25.csv $options -f $file
+
+#for file in `ls data/*.csv`
+#do
+#	l=`expr ${#file} - 9`
+#	f=${file:5:$l}
+#	for init in ${inits[@]}
+#	do
+#		./fold.sh test/$alg-$f-$init.csv $options -f $file -init $init
+#	done
+#done
